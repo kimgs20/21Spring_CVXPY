52674bd3a893ee60db73142c90522de17ca1252c
kimgs <kimgeunsukgs@gmail.com>
2021-05-19 20:08:49 +0900

    what is CVXPY?

--- cvxpy_01.py
+++ cvxpy_01.py
@@ -20,10 +20,30 @@ prob.solve()  # returns the optimal value
 print("status: ", prob.status)
 print("optimal value", prob.value)
 print("optimal var", x.value, y.value)
-
+print()
 """
 Result
 status:  optimal
 optimal value 1.0  # The value of obj fn. J(x, y) = 1
 optimal var 1.0 1.570086213240983e-22  # x=1, y=0
+"""
+
+# Probelms are immutable: they cannot be changed after they are created.
+# To change objective or constraints, create a new problem.
+
+# Replace the objective
+prob2 = cp.Problem(cp.Maximize(x + y), prob.constraints)
+print("optimal value of prob2", prob2.solve())  
+
+# value of prob.solve() is prob.value(optimal value of obj fn)
+
+
+# Replace the constrain (x + y == 1)
+constraints = [x + y <= 3] + prob2.constraints[1:]
+prob3 = cp.Problem(prob2.objective, constraints)
+print("optimal value of prob3", prob3.solve())
+"""
+Result
+optimal value of prob2 0.9999999999945575
+optimal value of prob3 2.9999999999746754
 """
